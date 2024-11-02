ff1755dd0d3b4724891ea428e4a33e50793d7abc
me <you@example.com>
2024-11-01 18:11:55 -0700

    updated

--- debian.sh
+++ debian.sh
@@ -7,9 +7,15 @@ sudo apt remove -y firefox-esr
 
 #Theme changes
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+gnome-extensions enable ubuntu-appindicators@ubuntu.com
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 gsettings set org.gnome.desktop.interface icon-theme "Yaru-purple-dark"
 dconf write /org/gnome/shell/extensions/user-theme/name "'Yaru-purple-dark'"
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gnome-extensions enable dash-to-dock@micxgx.gmail.com
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'LEFT'
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 42
 
 #Flatpaks
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
