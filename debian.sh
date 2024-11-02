977b9ccfd9d0a1085475d2c9fa6d064467b452b7
me <you@example.com>
2024-11-01 21:58:55 -0700

    added an update alias

--- debian.sh
+++ debian.sh
@@ -6,10 +6,11 @@
 sudo apt install -y gnome-core command-not-found flatpak gnome-shell-extension-dashtodock git yaru-theme-icon yaru-theme-gnome-shell gnome-shell-extension-appindicator gnome-tweaks
 sudo apt remove -y firefox-esr
 
-#theme changes
+#other changes
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 echo "PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin" >> ~/.bashrc
+echo "alias update="sudo apt update && sudo apt upgrade -y && flatpak update -y"" >> ~/.bashrc
 gnome-extensions enable ubuntu-appindicators@ubuntu.com
 gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 gsettings set org.gnome.desktop.interface icon-theme "Yaru-purple-dark"
