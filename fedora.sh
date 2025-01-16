70456e0144b2c4e62c5947f7b1a2b1b1ab64a592
me <you@example.com>
2025-01-15 19:52:47 -0800

    Added header

--- fedora.sh
+++ fedora.sh
@@ -4,17 +4,17 @@
 # Packages
 echo "defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf
 sudo dnf install bash-completion gnome-shell-extension-dash-to-dock gnome-extensions-app vlc https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf remove totem
-sudo dnf update
+sudo dnf remove totem -y
+sudo dnf update -y
 # Proprietary Stuff
-sudo dnf swap ffmpeg-free ffmpeg --allowerasing
-sudo dnf install libdvdcss rpmfusion-free-release-tainted
-sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+sudo dnf install libdvdcss rpmfusion-free-release-tainted -y
+sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 read -n1 -p "Would you want hardware accelerated codecs? Please select [I]ntel, [A]md, [N]vidia or [S]kip" doit 
 case $doit in  
-  a|A) sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld && sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld && sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686 && sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686 ;; 
-  i|I) sudo dnf install intel-media-driver ;;
-  n|N) sudo dnf install libva-nvidia-driver ;;
+  a|A) sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y && sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y && sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686 -y && sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686 -y ;; 
+  i|I) sudo dnf install intel-media-driver -y ;;
+  n|N) sudo dnf install libva-nvidia-driver -y ;;
   *) echo Skipping ;; 
 esac
 
