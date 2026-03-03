71558b4465526ee507e903b60f37cc535a193eb4
SandFrog <68484527+SandFrog@users.noreply.github.com>
2026-03-02 18:37:10 -0800

    Update debian.sh for Debian 13

--- linux/debian.sh
+++ linux/debian.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
-#last tested on Debian 12
-#uncheck all boxes
-#This may not work, sudo expires
+#Last tested on Debian 13
+#At software selection, select: Debian desktop environment, GNOME, standard system utilities
 
-#packages
-sudo apt update
-sudo apt install -y gnome-core command-not-found flatpak gnome-shell-extension-dashtodock git yaru-theme-icon yaru-theme-gnome-shell gnome-shell-extension-appindicator gnome-tweaks net-tools software-properties-common
-sudo apt purge -y firefox-esr
-sudo apt-add-repository --component non-free contrib non-free-firmware -y
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-#other changes
-gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-echo "PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin" >> ~/.bashrc
-echo "alias update=\"sudo apt update && sudo apt upgrade -y && flatpak update -y\"" >> ~/.bashrc
-gnome-extensions enable ubuntu-appindicators@ubuntu.com
-gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
-gsettings set org.gnome.desktop.interface icon-theme "Yaru-purple-dark"
-dconf write /org/gnome/shell/extensions/user-theme/name "'Yaru-purple-dark'"
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
-gnome-extensions enable dash-to-dock@micxgx.gmail.com
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'LEFT'
-gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 42
-gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
-
-sudo rm /etc/network/interfaces
+if ! gnome-extensions enable ubuntu-appindicators@ubuntu.com &>/dev/null; then
+    #Packages
+    sudo apt update
+    sudo apt install -y command-not-found git flatpak gnome-software-plugin-flatpak \
+        gnome-shell-extension-dashtodock gnome-shell-extension-appindicator gnome-tweaks
+    sudo apt purge -y firefox-esr totem showtime
+    sudo sed -i.old '/^deb\(-src\)\? / s/ main.*/ main contrib non-free non-free-firmware/' /etc/apt/sources.list
+    sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    flatpak install flathub -y io.github.celluloid_player.Celluloid org.mozilla.firefox
+    
+    if whiptail --title "Steam Setup" --yesno "Do you want to install Steam and 32-bit libraries?" 0 0; then
+        echo "Installing Steam..."
+        sudo dpkg --add-architecture i386
+        sudo apt update
+        sudo apt install -y steam-installer
+    else
+        echo "Skipping Steam."
+    fi
+    
+    #Hide grub
+    sudo sed -i \
+        -e 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' \
+        -e '/^GRUB_TIMEOUT=0/a GRUB_TIMEOUT_STYLE=hidden' /etc/default/grub
+    
+    #Other changes
+    echo "PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin" >> ~/.bashrc
+    echo "alias update=\"sudo apt update && sudo apt upgrade -y && flatpak update -y\"" >> ~/.bashrc
+    
+    echo -e "\e[1;97;44mPlease log out, log back in and re-run this script to enable extensions.\e[0m"
+else
+    #Gnome
+    gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+    gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+    gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+    gnome-extensions enable ubuntu-appindicators@ubuntu.com
+    gnome-extensions enable dash-to-dock@micxgx.gmail.com
+    gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+    gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+    gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 42
+    gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
+fi
